bd828a39a6c93e4a261e5afb654816583ebf7a74
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-21 20:50:43 +0300

    Boolean type, some integer and string methods, diividing two int or double nums exercises

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -14,12 +14,14 @@ var phoneNumber = "test"
 
 name.uppercased()
 phoneNumber.append(contentsOf: "1")
+name.count
 
 
 // Integer - int
 
 var num = 10
 num.isMultiple(of: 2)
+
 
 var dataFromServer = 50
 var data_from_server = 50
@@ -28,11 +30,30 @@ var data_from_server = 50
 let myNumber = 50
 print(myNumber)
 
+let otherFirstNumber = 20
+let otherSecondNumber = 6
+
+otherFirstNumber/otherSecondNumber
+
+
 // Let is constant
 //myNumber = 30
 
 // Double
 let pi = 3.1454364
+
+let doubleNum = 20.0
+let doublenum2 = 6.0
+
+doubleNum/doublenum2
+
+
+// Boolean
+var myBoolean = true
+myBoolean = false
+
+
+
 
 
 
