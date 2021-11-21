ae4f040342473e128881d15d67d9dfdd3ab8a136
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-21 20:40:48 +0300

    Integer,Double,String and let keywords learned.

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,17 +1,38 @@
 import UIKit
 
+
+
+//String
 var name = "İbrahim"
 var surname = "Demirci"
 
 name = "Changed as ibrahim"
 print(name)
 
-// Comment Line
 
 var phoneNumber = "test"
+
+name.uppercased()
+phoneNumber.append(contentsOf: "1")
+
+
+// Integer - int
+
+var num = 10
+num.isMultiple(of: 2)
+
 var dataFromServer = 50
 var data_from_server = 50
 
+
+let myNumber = 50
+print(myNumber)
+
+// Let is constant
+//myNumber = 30
+
+// Double
+let pi = 3.1454364
 
 
 
