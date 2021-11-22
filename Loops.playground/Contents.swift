fb6c05c61a8c90ebed60d756712466fb8575dae4
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-22 14:43:29 +0300

    While loop

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// Loops
+
+// While Loop
+
+5 == 5
+5 == 4
+5 != 6
+3 < 9
+3 < 2
+2 >= 2
+
+
+var x = 0
+
+x = x + 1
+x += 1
+
+
+while x < 10 {
+    print(x)
+    x = x + 1
+    }
