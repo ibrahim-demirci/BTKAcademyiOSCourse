47234fb2e8678fd6ee72485c9f2a1020d07a7160
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-24 11:51:38 +0300

    Scope learned and Calculator events added

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -9,34 +9,85 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var result = 0
+    
+    
     @IBOutlet weak var firstText: UITextField!
     @IBOutlet weak var secondText: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
-    
+         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func addition(_ sender: Any) {
-        let first = Int(firstText.text!)!
-        let second = Int(secondText.text!)!
         
-        let res = first + second
-        resultLabel.text = String(res)
-
+        if let firstNum = Int(firstText.text!){
+            if let secondNum = Int(secondText.text!){
+                result = firstNum + secondNum
+                resultLabel.text = String(result)
+                
+            }else{
+                resultLabel.text = "Second field is not num!"
+            }
+            
+        }else{
+            resultLabel.text = "First field is not num! "
+        }
     }
     
     @IBAction func subtraction(_ sender: Any) {
-    
+        
+        if let firstNum = Int(firstText.text!){
+            if let secondNum = Int(secondText.text!){
+                result = firstNum - secondNum
+
+                resultLabel.text = String(result)
+
+            }else{
+                resultLabel.text = "Second field is not num!"
+
+            }
+        }else{
+            resultLabel.text = "First field is not num! "
+        }
+
     }
     
     @IBAction func division(_ sender: Any) {
-    
+        if let firstNum = Int(firstText.text!){
+            if let secondNum = Int(secondText.text!){
+                
+                result = firstNum / secondNum
+                resultLabel.text = String(result)
+
+            }else{
+                resultLabel.text = "Second field is not num!"
+
+            }
+        }else{
+            resultLabel.text = "First field is not num! "
+
+        }
     }
     
     @IBAction func multiplication(_ sender: Any) {
-    
+        
+        if let firstNum = Int(firstText.text!){
+            if let secondNum = Int(secondText.text!){
+                
+                result = firstNum * secondNum
+                resultLabel.text = String(result)
+
+            }else{
+                
+                resultLabel.text = "Second field is not num!"
+            }
+        }else{
+            
+            resultLabel.text = "First field is not num! "
+        }
     }
 }
 
