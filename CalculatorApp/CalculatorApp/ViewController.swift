2b8323f805676fe86b643d05c2130c35f17b7f71
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-24 11:31:01 +0300

    UI components binded to code

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func addition(_ sender: Any) {
+        let first = Int(firstText.text!)!
+        let second = Int(secondText.text!)!
+        
+        let res = first + second
+        resultLabel.text = String(res)
 
+    }
+    
+    @IBAction func subtraction(_ sender: Any) {
+    
+    }
+    
+    @IBAction func division(_ sender: Any) {
+    
+    }
+    
+    @IBAction func multiplication(_ sender: Any) {
+    
+    }
 }
 
