2bb510ea5b12a27cbed42778d89ef9c682952b4d
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-24 11:14:49 +0300

    Calculater UI designed

---
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by İbrahim Demirci on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
