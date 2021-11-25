8eb3b384f7626dbb447f38ec45adf091ab28432d
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-25 23:20:55 +0300

    ViewControllers added, labels binded

---
+++ CitiesProject/CitiesProject/DetailsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsViewController.swift
+//  CitiesProject
+//
+//  Created by İbrahim Demirci on 25.11.2021.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imagePath: UILabel!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var cityRegion: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ CitiesProject/CitiesProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CitiesProject
+//
+//  Created by İbrahim Demirci on 25.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
