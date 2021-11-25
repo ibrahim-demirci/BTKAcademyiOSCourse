78554300a5bb735934075d2cbd524666f40bf336
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-25 14:08:23 +0300

    Button added as manually and function added

--- LayoutProject/LayoutProject/ViewController.swift
+++ LayoutProject/LayoutProject/ViewController.swift
@@ -16,13 +16,29 @@ class ViewController: UIViewController {
         let width = view.frame.size.width
         let height = view.frame.size.height
         
+        
+        //Label
         let myLabel = UILabel()
         myLabel.text = "Manuel Label"
         myLabel.textAlignment = .center
-        myLabel.frame = CGRect(x: width * 0.1, y: height * 0.5 - 50, width: width * 0.8, height: 100)
+        myLabel.frame = CGRect(x: width * 0.1, y: height * 0.5 - 50, width: width * 0.8, height: 300)
         view.addSubview(myLabel)
         
         
+        //Button
+        let myButton = UIButton()
+        myButton.setTitle("My Button", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.orange, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.5 - 100, y: height * 0.65, width: 200, height: 100)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(ViewController.buttonFunc), for: UIControl.Event.touchUpInside)
+    }
+    
+    
+    @IBAction func buttonFunc(){
+        
+        print("Button Clicked")
     }
     
 
