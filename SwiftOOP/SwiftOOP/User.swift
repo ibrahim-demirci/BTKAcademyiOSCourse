381c31c849cca1f3c8c9ea6816009a890cf5985a
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-25 22:38:50 +0300

    Classes

---
+++ SwiftOOP/SwiftOOP/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  SwiftOOP
+//
+//  Created by İbrahim Demirci on 25.11.2021.
+//
+
+import Foundation
+
+class User {
+    
+    var name = ""
+    var age = 0
+    var work = ""
+    
+}
---
+++ SwiftOOP/SwiftOOP/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  SwiftOOP
+//
+//  Created by İbrahim Demirci on 25.11.2021.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+let atil = User()
+
+atil.name = "Atil"
+atil.age = 30
+atil.work = "Engineer"
+
+let katarina = User()
+
