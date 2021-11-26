e8fea9dc8777adff3de297713f66737810427397
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-26 12:02:48 +0300

    Add button added on navigatonBar and added GestureRecognizer on VC

--- NoteAppWithCoreData/NoteAppWithCoreData/DetailsViewController.swift
+++ NoteAppWithCoreData/NoteAppWithCoreData/DetailsViewController.swift
@@ -10,24 +10,28 @@ import UIKit
 class DetailsViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var title: UITextField!
-    @IBOutlet weak var price: UITextField!
-    @IBOutlet weak var size: UITextField!
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var sizeField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let gestureRec = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(gestureRec)
+        
+    }
+    
+    
+    
+    @objc func closeKeyboard(){
+        
+        view.endEditing(true)
+        
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
 }
