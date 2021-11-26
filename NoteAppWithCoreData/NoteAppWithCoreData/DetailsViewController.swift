0b15d684eb226db2fb243bec7e22fb2a68695c6f
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-26 13:02:12 +0300

    ImagePicker added

--- NoteAppWithCoreData/NoteAppWithCoreData/DetailsViewController.swift
+++ NoteAppWithCoreData/NoteAppWithCoreData/DetailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleField: UITextField!
@@ -21,14 +21,32 @@ class DetailsViewController: UIViewController {
         let gestureRec = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
         view.addGestureRecognizer(gestureRec)
         
+        imageView.isUserInteractionEnabled = true
+        let imageGesture = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageGesture)
+        
     }
     
     
     
     @objc func closeKeyboard(){
-        
         view.endEditing(true)
+    }
+    
+    @objc func selectImage(){
         
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
         
     }
     
