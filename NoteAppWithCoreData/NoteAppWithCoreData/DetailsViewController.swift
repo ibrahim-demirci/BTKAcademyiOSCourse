23ad9cd5452ee5a79d3a5e4c29328e2c3ac37f51
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-26 11:45:00 +0300

    UI components replaces on screen and binded to code

---
+++ NoteAppWithCoreData/NoteAppWithCoreData/DetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsViewController.swift
+//  NoteAppWithCoreData
+//
+//  Created by İbrahim Demirci on 26.11.2021.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var title: UITextField!
+    @IBOutlet weak var price: UITextField!
+    @IBOutlet weak var size: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
