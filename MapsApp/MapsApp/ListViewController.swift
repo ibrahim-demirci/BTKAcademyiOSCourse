a12cff2a6a20ef4012118caaf46503d3ed3f85e8
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-11-28 22:13:06 +0300

    Fetch datas from CoreData is OK

--- MapsApp/MapsApp/ListViewController.swift
+++ MapsApp/MapsApp/ListViewController.swift
@@ -6,10 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class ListViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var titlesArray = [String]()
+    var idArray = [UUID]()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,9 +21,49 @@ class ListViewController: UIViewController ,UITableViewDelegate,UITableViewDataS
         tableView.dataSource = self
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(goToMaps))
+        
+        
+        
+        getDatasFromCoreData()
 
-        // Do any additional setup after loading the view.
     }
+    
+    
+    func getDatasFromCoreData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Place")
+        request.returnsObjectsAsFaults = false
+        
+        
+        do{
+            let results = try context.fetch(request)
+            
+            if results.count > 0{
+                
+                titlesArray.removeAll(keepingCapacity: false)
+                idArray.removeAll(keepingCapacity: false)
+                
+                for item in results as! [NSManagedObject]{
+                    if let title =  item.value(forKey: "title") as? String{
+                        titlesArray.append(title)
+                    }
+                    if let id =  item.value(forKey: "id") as? UUID{
+                        idArray.append(id)
+                    }
+                }
+                tableView.reloadData()
+            }
+            
+        }catch{
+            print("fail to fetch datas")
+        }
+        
+        
+        
+    }
+    
     
     @objc func goToMaps(){
         performSegue(withIdentifier: "toMapsVC", sender: nil)
@@ -28,13 +72,13 @@ class ListViewController: UIViewController ,UITableViewDelegate,UITableViewDataS
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return titlesArray.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = titlesArray[indexPath.row]
         return cell
     }
     
