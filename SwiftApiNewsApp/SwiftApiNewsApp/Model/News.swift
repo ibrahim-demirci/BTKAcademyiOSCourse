46d36851dd8ca38d36a7e215b8e3a2ef9d726288
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-02 12:20:48 +0300

    News model created

---
+++ SwiftApiNewsApp/SwiftApiNewsApp/Model/News.swift
@@ -0,0 +1,20 @@
+//
+//  News.swift
+//  SwiftApiNewsApp
+//
+//  Created by İbrahim Demirci on 2.12.2021.
+//
+
+import Foundation
+
+
+struct News : Decodable{
+    
+    let title : String
+    let story : String
+    
+    
+    //GET & POST
+    
+    
+}
