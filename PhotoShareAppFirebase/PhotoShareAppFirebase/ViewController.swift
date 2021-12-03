9532ff0c063738ca525bec456cdee33832ae3aab
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-03 15:04:34 +0300

    Sign In/Up page designed and Tapbar controller added

--- PhotoShareAppFirebase/PhotoShareAppFirebase/ViewController.swift
+++ PhotoShareAppFirebase/PhotoShareAppFirebase/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func signIn(_ sender: Any) {
+    }
+    @IBAction func signUp(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
 
 }
 
