77fef71afaed86b468313234e1bd4f79e6c8c92a
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-03 15:49:47 +0300

    Create user added, validations added in sign form

--- PhotoShareAppFirebase/PhotoShareAppFirebase/ViewController.swift
+++ PhotoShareAppFirebase/PhotoShareAppFirebase/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -20,9 +21,35 @@ class ViewController: UIViewController {
     @IBAction func signIn(_ sender: Any) {
     }
     @IBAction func signUp(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+        if emailTextField.text != "" && passwordTextField.text != ""{
+            //Create User
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { authResult, error in
+                
+                if error != nil{
+                    self.errorMessage(titleInput: "Auth Error", messageInput: error?.localizedDescription ?? "Try Again")
+                    
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+            
+        }else{
+            //Error
+            errorMessage(titleInput: "Fail", messageInput: "Enter email and password")
+        }
     }
     
+    
+    func errorMessage(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
+        
+    }
 
 }
 
