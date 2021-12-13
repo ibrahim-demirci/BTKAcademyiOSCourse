503aa3bfcff276c8ded3ed77f80e16444743b253
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-13 18:10:18 +0300

    Pick image added and button active inactive added

--- PhotoShareAppParse/PhotoShareAppParse/UploadViewController.swift
+++ PhotoShareAppParse/PhotoShareAppParse/UploadViewController.swift
@@ -7,14 +7,51 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    @IBOutlet weak var shareButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let keyboardRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(keyboardRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageGesture = UITapGestureRecognizer(target: self, action: #selector(pickImage))
+        imageView.addGestureRecognizer(imageGesture)
+        
+        
+        shareButton.isEnabled = false
+        
+        
     }
     
-
+    @IBAction func shareClicked(_ sender: Any) {
+        
+        
+    }
+    
+    
+    @objc func pickImage(){
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        shareButton.isEnabled = true
+        
+    }
+    @objc func closeKeyboard(){
+        view.endEditing(true)
+    }
+    
 
 }
