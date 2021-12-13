b838688567f15c6eae9844d7e9889199528efb54
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-13 16:15:25 +0300

    Sign up event added

--- PhotoShareAppParse/PhotoShareAppParse/ViewController.swift
+++ PhotoShareAppParse/PhotoShareAppParse/ViewController.swift
@@ -10,8 +10,8 @@ import Parse
 
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +28,36 @@ class ViewController: UIViewController {
     }
     
     @IBAction func signUpClicked(_ sender: Any) {
+        
+        
+        if usernameField.text != "" && passwordField.text != ""{
+            
+            let user = PFUser()
+            user.username = usernameField.text
+            user.password = passwordField.text
+            
+            user.signUpInBackground { isSuccess, error in
+                
+                if error != nil{
+                    self.showAlert(title: "Error", message: error?.localizedDescription ?? "Error when signUp")
+                } else {
+                    self.performSegue(withIdentifier: "toTabBar", sender: nil)
+                }
+            }
+        } else {
+            showAlert(title: "Empty", message: "Cant be empty username or password")
+        }
+        
+    }
+    
+    func showAlert(title : String, message : String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        
+        self.present(alert, animated: true, completion: nil)
+        
     }
 }
 
