ce7b2ab09f11c02effb34d0dbf7a857c33063b7f
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-13 13:29:53 +0300

    Parse connected to the app in AppDelegate.swift

--- PhotoShareAppParse/PhotoShareAppParse/AppDelegate.swift
+++ PhotoShareAppParse/PhotoShareAppParse/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+              $0.applicationId = "NmwkrJK2pG4qEeBSUKXIbWQmUTAUYj9JY8zrQcGP"
+              $0.clientKey = "NVWSYVel6rpWqghPrtARGmW8Z7pYlj6UkU9JCsBF"
+              $0.server = "https://parseapi.back4app.com"
+          }
+          Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 
