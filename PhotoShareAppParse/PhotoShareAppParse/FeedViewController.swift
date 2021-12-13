005eba8dd8ce36412cd081aeceb3b424d0bbb95e
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-13 19:23:18 +0300

    Get Images using Parse library added and app finished

--- PhotoShareAppParse/PhotoShareAppParse/FeedViewController.swift
+++ PhotoShareAppParse/PhotoShareAppParse/FeedViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
     
@@ -21,6 +22,8 @@ class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDat
         tableView.dataSource = self
         tableView.delegate = self
         
+        getPosts()
+        
     }
     
     
@@ -29,9 +32,16 @@ class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDat
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FeedTableViewCell
         cell.usernameLabel.text = postsArray[indexPath.row].username
         cell.commentLabel.text = postsArray[indexPath.row].comment
-        
-        
-        
+//        cell.postImageView.image = UIImage(named: "white")
+        postsArray[indexPath.row].image.getDataInBackground { data, error in
+            if error == nil {
+                if let data = data {
+                    print("resim yükleniyor")
+                    cell.postImageView.image = UIImage(data: data)
+                }
+
+            }
+        }
         return cell
         
     }
@@ -40,5 +50,53 @@ class FeedViewController: UIViewController , UITableViewDelegate, UITableViewDat
         return postsArray.count
     }
     
+    
+    @objc func getPosts(){
+        
+        let query = PFQuery(className: "Posts")
+        query.addDescendingOrder("createdAt")
+        
+        query.findObjectsInBackground { objects, error in
+            if error != nil{
+                self.showAlert(title: "Error", message: error?.localizedDescription ?? "Error when loading posts ")
+            } else {
+                if objects != nil {
+                    if objects!.count > 0 {
+                        self.postsArray.removeAll(keepingCapacity: false)
+                        
+                        for object in objects!{
+                            if let username = object.object(forKey: "username") as? String{
+                                if let comment = object.object(forKey: "comment") as? String{
+                                    if let image = object.object(forKey: "image") as? PFFileObject{
+                                        let post = PostModel(username: username, comment: comment, image: image)
+                                        self.postsArray.append(post)
+                                    }
+                                    
+                                }
+                            }
+                            
+                        }
+                        print(self.postsArray)
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    func showAlert(title : String, message : String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getPosts), name: NSNotification.Name(rawValue: "post"), object: nil)
+    }
 
 }
